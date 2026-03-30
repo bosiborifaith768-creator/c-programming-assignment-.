983588a37fc1f144f2bdceb28be884e5cd7ac80e
bosiborifaith768-creator <bosiborifaith768@gmail.com>
2026-03-30 06:11:31 +0300

    Add files via upload

---
+++ units.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	//program to display units offered this semester
+	
+	printf("Units Offered This semester:\n");
+	
+	printf("1. Introduction to Programming\n");
+	printf("2. Data Structures\n");
+	printf("3. Computer Networks\n");
+	printf("4. Dtabase Systems\n");
+	printf("5. Software Engineering\n");
+	
+	return 0;
+}
